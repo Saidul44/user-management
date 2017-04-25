4dd16edc1dedaa09eb3fe424a4ae14b9d9641d2e
saidul <saidul@grameentek.com>
2017-04-26 04:48:24 +0600

    vendor remove

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Project
+                    Laravel
                 </div>
 
                 <div class="links">
